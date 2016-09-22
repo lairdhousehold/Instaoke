026ab3886931c2bd2df5ec789c0f876606d6f9af
Jammy Laird <jammylaird@yahoo.com>
2016-09-22 00:37:05 -0500

    styling half way

--- app/factories/AuthFactory.js
+++ app/factories/AuthFactory.js
@@ -5,12 +5,11 @@ app.factory("AuthFactory", function($window) {
 
     let createUser = function(userObj) {
         console.log('userObj ', userObj);
-        return firebase.auth().createUserWithEmailAndPassword(userObj.email, userObj.password) //here we create our own method on a built-in FB method (auth) //CAN ONLY PASS IN TWO ARGUMENTS HERE
+        return firebase.auth().createUserWithEmailAndPassword(userObj.email, userObj.password)
             .catch(function(error) {
                 let errorCode = error.code;
                 let errorMessage = error.message;
             })
-            //returning a promise, but we don't have to write it, because they're already embedded as Firebase methods. We have universal access to firebase through link in index.html
     }
 
     let loginUser = function(userObj) {
